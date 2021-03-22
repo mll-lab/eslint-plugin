2ebfc1326f81e4894d047bc0623773523e564b2b
Benedikt Franke <benedikt.franke@mll.com>
2021-03-22 16:54:57 +0100

    feat: Add rule no-global-styles

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 import { castToBoolean } from './rules/cast-to-boolean';
 import { noColorLiterals } from './rules/no-color-literals';
+import { noGlobalStyles } from './rules/no-global-styles';
 
 const rules = {
   'cast-to-boolean': castToBoolean,
   'no-color-literals': noColorLiterals,
+  'no-global-styles': noGlobalStyles,
 };
 
 const recommendedRules = {
   '@mll-lab/cast-to-boolean': 'error',
   '@mll-lab/no-color-literals': 'error',
+  '@mll-lab/no-global-styles': 'error',
 };
 
 export = {
